cd53870417f1c19b521c33b3acd548fcca757157
Nolan <mailbox@inolan.ru>
2012-06-26 22:36:06 +0300

    [Общее]: Смена иконок и некоторой информации.

--- revision.h.in.cmake
+++ revision.h.in.cmake
@@ -2,9 +2,9 @@
 #define __REVISION_H__
  #define _HASH                      "@rev_hash@"
  #define _DATE                      "@rev_date@"
- #define VER_COMPANYNAME_STR        "TrinityCore Developers"
- #define VER_LEGALCOPYRIGHT_STR     "(c)2008-2012 TrinityCore"
- #define VER_FILEVERSION            0,0,0
+ #define VER_COMPANYNAME_STR        "Living Legends Development"
+ #define VER_LEGALCOPYRIGHT_STR     "(c)2012 Living Legends"
+ #define VER_FILEVERSION            1,0,0
  #define VER_FILEVERSION_STR        "@rev_date@ (@rev_hash@)"
  #define VER_PRODUCTVERSION         VER_FILEVERSION
  #define VER_PRODUCTVERSION_STR     VER_FILEVERSION_STR
