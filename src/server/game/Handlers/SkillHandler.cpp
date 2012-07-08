44aeff6bc877121350306a91d46bac8b52ece421
Nolan <mailbox@inolan.ru>
2012-07-08 05:18:54 +0300

    [Профессии]: Не разучивать профессию при нажати кнопки "отказаться от профессии" в клиенте.

--- src/server/game/Handlers/SkillHandler.cpp
+++ src/server/game/Handlers/SkillHandler.cpp
@@ -88,5 +88,5 @@ void WorldSession::HandleUnlearnSkillOpcode(WorldPacket & recv_data)
 {
     uint32 skill_id;
     recv_data >> skill_id;
-    GetPlayer()->SetSkill(skill_id, 0, 0, 0);
+    GetPlayer()->TextEmote("думает что не стоит отказываться от этой профессии. Она ещё может пригодиться.");
 }
